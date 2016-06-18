b80741c858329d0067416ec30524f9be36587523
mowrix <mugomorr2@gmail.com>
2016-06-18 15:59:52 +0300

    flights Booking

--- resources/views/pages/book.blade.php
+++ resources/views/pages/book.blade.php
@@ -1,20 +1,42 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Booking</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+                        {{ csrf_field() }}
 
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">From</label>
 
+                            <div class="col-md-6">
+                                <input id="email" type="text" class="form-control" name="email" value="{{ old('email') }}">
+                            </div>
+                        </div>
 
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">To</label>
 
-{{ Form::open(array('url' => 'pages.book')) }}
+                            <div class="col-md-6">
+                                <input id="password" type="text" class="form-control" name="password">
+                            </div>
+                        </div>
 
-    {{ Form::label('first_name', 'First Name') }}
-    {{ Form::text('first_name', 'Taylor Otwell') }}
-
-{{ Form::close() }}
-
-
-
-
-
-
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
